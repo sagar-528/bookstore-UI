80c603e439c4c9da4aa3bf1c5bfe72f9250ac374
sagar-528 <gupta.sagar528@gmail.com>
2020-06-17 20:39:26 +0530

    ADDED: Project Structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AppRouter from './AppRouter';
+
+
+function App() {
+  return (
+      <div>
+
+      </div>
+    );
+}
+
+export default App;
