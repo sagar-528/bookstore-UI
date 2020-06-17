d96fe7842f26bffdb5746d0d28dab1f1e6835d44
sagar-528 <gupta.sagar528@gmail.com>
2020-06-17 21:42:21 +0530

    ADDED: Routed And Template of Cart

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,6 +1,9 @@
 import DashBoard from './Component/DashBoard/DashBoard'
 import { BrowserRouter, Route, Switch, Router} from 'react-router-dom'
 import React, { Component } from 'react'
+import Cart from './Component/Cart/Cart'
+import WishList from './Component/WishList/WishList'
+import ConfirmOrder from './Component/ConfirmOrder/ConfirmOrder'
 
 class AppRouter extends Component {
     render() {
@@ -9,6 +12,9 @@ class AppRouter extends Component {
                 <div>
                     <Switch>
                         <Route path="/" component={DashBoard} exact="true" /> 
+                        <Route path="/Cart" component={Cart} />
+                        <Route path="/WishList" component={WishList} /> 
+                        <Route path="/ConfirmOrder" component={ConfirmOrder} />
                     </Switch>      
                 </div>
             </BrowserRouter>
