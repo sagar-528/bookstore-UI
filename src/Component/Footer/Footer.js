1c68d3ce299618dfdf91790bcb814d63894f7b77
sagar-528 <gupta.sagar528@gmail.com>
2020-06-17 21:21:30 +0530

    ADDED: UI of Footer.

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class Footer extends Component {
-    render() {
-        return (
-            <div>
-                Footer
-            </div>
-        )
-    }
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+
+    title: {
+        flexGrow: 1,
+        fontFamily: 'Arial, Helvetica, sans- serif',
+        fontSize: '12px',
+        marginLeft: '80px'
+    },
+    
+    footer: {
+        backgroundColor: "#4e342e",
+    },
+}));
+function Footer() {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+        <AppBar position="static" className={classes.footer}>
+            <Toolbar>
+                <Typography variant="h8" className={classes.title}>
+                    Copyright © 2020, Bookstore Private Limited. All Rights Reserved.
+      </Typography>
+            </Toolbar>
+        </AppBar>
+    </div>
+    )
 }
 
 export default Footer
