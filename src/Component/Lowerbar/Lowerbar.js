2947c324b43ad2306ac8d7c5b847b9daba835a86
sagar-528 <gupta.sagar528@gmail.com>
2020-06-17 21:15:24 +0530

    ADDED: UI of Lowerbar

--- src/Component/Lowerbar/Lowerbar.js
+++ src/Component/Lowerbar/Lowerbar.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
 
 export class Lowerbar extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+        }
+    }
+
     render() {
         return (
-            <div>
-                Lowerbar
-            </div>
+            <p>
+            <div >
+                <Toolbar>
+                    <Typography edge="start" variant="h6" >
+                        Books (52 items)
+                    </Typography>
+                    </Toolbar>
+               </div>
+            </p>
         )
     }
 }
