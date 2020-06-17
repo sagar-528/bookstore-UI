2fca43065ec0e108c19d0494d01ba7776d0562bb
sagar-528 <gupta.sagar528@gmail.com>
2020-06-17 21:17:37 +0530

    ADDED: UI of Pagination

--- src/Component/Pagination/Pagination.js
+++ src/Component/Pagination/Pagination.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 
 export class Pagination extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    }
+  }
+  
+  displayButton = () => {
+    let result = this.state.data.map(o => {
+      return o
+    })
+    this.setState({ data: result });
+  }
+
   render() {
     return (
       <div>
-        Pagination
+         <IconButton color="primary" component="span">
+              <KeyboardArrowLeftIcon />
+            </IconButton>
+            {this.state.data.map(o => (
+              <Button color="black">{o}</Button>
+            ))}
+            <IconButton color="primary" component="span" style={{ border: '2px' }}>
+              <KeyboardArrowRightIcon />
+            </IconButton>
       </div>
     )
   }
